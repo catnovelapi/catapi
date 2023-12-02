762109abc2af91c0433f3ff050200d0278699f87
cat <catnovelapi123@gmail.com>
2023-12-02 14:37:27 +0800

    optimize code

--- catapi/api_client.go
+++ catapi/api_client.go
@@ -1,19 +1,14 @@
 package catapi
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/sha256"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"github.com/catnovelapi/builder"
+	"github.com/catnovelapi/catapi/catapi/decrypt"
 	"github.com/tidwall/gjson"
 )
 
 type CiweimaoRequest struct {
-	Version       string
-	Account       string
 	BuilderClient *builder.Client
 }
 
@@ -31,7 +26,7 @@ func (request *CiweimaoRequest) Post(url string, data map[string]any) (gjson.Res
 		return gjson.Result{}, errors.New("responseText is empty, please check your network")
 	}
 	if !gjson.Valid(responseText) {
-		responseText, err = request.DecodeEncryptText(response.String(), "")
+		responseText, err = decrypt.DecodeEncryptText(response.String(), "")
 		if err != nil {
 			return gjson.Result{}, fmt.Errorf("decode error: %s", err.Error())
 		}
@@ -41,47 +36,4 @@ func (request *CiweimaoRequest) Post(url string, data map[string]any) (gjson.Res
 		return gjson.Result{}, fmt.Errorf("response error: %s", gjsonResponseText.Get("tip").String())
 	}
 	return gjsonResponseText, nil
-}
-
-// SHA256 sha256 编码
-func SHA256(data []byte) []byte {
-	ret := sha256.Sum256(data)
-	return ret[:]
-}
-
-func aesDecrypt(EncryptKey string, ciphertext []byte) ([]byte, error) {
-	block, err := aes.NewCipher(SHA256([]byte(EncryptKey))[:32])
-	if err != nil {
-		return nil, err
-	}
-	blockModel := cipher.NewCBCDecrypter(block, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-	plainText := make([]byte, len(ciphertext))
-	blockModel.CryptBlocks(plainText, ciphertext)
-	plainText = PKCS7UnPadding(plainText)
-	return plainText, nil
-}
-
-// PKCS7UnPadding 对齐
-func PKCS7UnPadding(plainText []byte) []byte {
-	length := len(plainText)
-	unpadding := int(plainText[length-1])
-	return plainText[:(length - unpadding)]
-}
-
-func (request *CiweimaoRequest) DecodeEncryptText(str string, decodeKey string) (string, error) {
-	if decodeKey == "" {
-		decodeKey = "zG2nSeEfSHfvTCHy5LCcqtBbQehKNLXn"
-	}
-	decoded, err := base64.StdEncoding.DecodeString(str)
-	if err != nil {
-		return "", fmt.Errorf("base64 decode error: %s", err.Error())
-	}
-	raw, err := aesDecrypt(decodeKey, decoded)
-	if err != nil {
-		return "", errors.New("解密失败,请检查解密密钥是否正确")
-	}
-	if len(raw) == 0 {
-		return "", errors.New("解密内容为空,请检查解密内容内容是否正确")
-	}
-	return string(raw), nil
 }
---
+++ catapi/decrypt/decrypt.go
@@ -0,0 +1,53 @@
+package decrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+)
+
+// SHA256 sha256 编码
+func SHA256(data []byte) []byte {
+	ret := sha256.Sum256(data)
+	return ret[:]
+}
+
+func aesDecrypt(EncryptKey string, ciphertext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(SHA256([]byte(EncryptKey))[:32])
+	if err != nil {
+		return nil, err
+	}
+	blockModel := cipher.NewCBCDecrypter(block, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	plainText := make([]byte, len(ciphertext))
+	blockModel.CryptBlocks(plainText, ciphertext)
+	plainText = PKCS7UnPadding(plainText)
+	return plainText, nil
+}
+
+// PKCS7UnPadding 对齐
+func PKCS7UnPadding(plainText []byte) []byte {
+	length := len(plainText)
+	unpadding := int(plainText[length-1])
+	return plainText[:(length - unpadding)]
+}
+
+func DecodeEncryptText(str string, decodeKey string) (string, error) {
+	if decodeKey == "" {
+		decodeKey = "zG2nSeEfSHfvTCHy5LCcqtBbQehKNLXn"
+	}
+	decoded, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return "", fmt.Errorf("base64 decode error: %s", err.Error())
+	}
+	raw, err := aesDecrypt(decodeKey, decoded)
+	if err != nil {
+		return "", errors.New("解密失败,请检查解密密钥是否正确")
+	}
+	if len(raw) == 0 {
+		return "", errors.New("解密内容为空,请检查解密内容内容是否正确")
+	}
+	return string(raw), nil
+}
